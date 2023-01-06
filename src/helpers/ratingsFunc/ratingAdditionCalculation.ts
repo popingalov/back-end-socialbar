6666d0a321461ffe2d318c4058528a879594b735
puerarongo <akhardin96@gmail.com>
2023-01-06 18:28:15 +0200

    add

--- src/helpers/ratingsFunc/ratingAdditionCalculation.ts
+++ src/helpers/ratingsFunc/ratingAdditionCalculation.ts
@@ -1,22 +1,22 @@
 import { defaultDict } from './defaultRating';
 import sumRatings from './sumRaiting';
 
-const ratingCalculation = (responseData, rating) => {
+const ratingAdditionCalculation = (responseData, rating) => {
   const { one, two, three, four, five, total } = responseData.ratings;
 
   const value = responseData.ratings[defaultDict[rating]];
   const ratingRequest = { [defaultDict[rating]]: value + 1 };
 
-  const oldValue = { one, two, three, four, five, ...ratingRequest };
+  const updateData = { one, two, three, four, five, ...ratingRequest };
   const countValue = {
     total: total + 1,
-    average: sumRatings(oldValue, total, rating),
+    average: sumRatings(updateData, total, rating),
   };
 
   return {
-    oldValue,
+    updateData,
     countValue,
   };
 };
 
-export default ratingCalculation;
+export default ratingAdditionCalculation;
