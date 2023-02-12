c4a9d3b351617678969a2b2fb5ca2e9247f8a60d
Oleksandr Balitskiy <popingalov@gmail.com>
2023-02-12 21:42:44 +0300

    return

--- src/globalDto/id.dto.ts
+++ src/globalDto/id.dto.ts
@@ -1,8 +1,7 @@
-import { IsMongoId, Length } from 'class-validator';
+import { IsMongoId } from 'class-validator';
 import { Types } from 'mongoose';
 
 export class IdDto {
-  @Length(24, 24)
-  // @IsMongoId()
+  @IsMongoId()
   readonly id: Types.ObjectId;
 }
