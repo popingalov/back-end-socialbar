126069b1839116c36309ec6b23b15b088533a87f
puerarongo <akhardin96@gmail.com>
2023-02-13 19:22:08 +0200

    fix2

--- src/domain/cocktails/shame/cocktails.schema.ts
+++ src/domain/cocktails/shame/cocktails.schema.ts
@@ -23,21 +23,18 @@ export class CocktailData {
 
   @Prop({
     type: [CocktailIngredientsSchema],
-    //required: true,
     ref: 'Ingredient',
   })
   ingredients: CocktailIngredients[];
 
   @Prop({
     type: CocktailRatingSchema,
-    //required: true,
     default: defaultRating,
   })
   ratings: CocktailRating;
 
   @Prop({
     type: [String],
-    //required: true,
   })
   category: string[];
 
@@ -48,25 +45,20 @@ export class CocktailData {
   })
   glass: Types.ObjectId;
 
-  @Prop({
-    //required: true,
-  })
+  @Prop({ type: String })
   title: string;
 
   @Prop({
     type: String,
-    //required: true,
   })
   description: string;
 
   @Prop({
     type: String,
-    //required: true,
   })
   recipe: string;
 
   @Prop({
-    //required: false,
     default:
       'https://media.istockphoto.com/id/942856796/uk/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D1%83%D0%B1%D0%B0-%D0%BB%D1%96%D0%B1%D1%80%D0%B5-%D0%BA%D0%BE%D0%BA%D1%82%D0%B5%D0%B9%D0%BB%D1%8C-%D0%BD%D0%B0-%D1%81%D1%96%D0%BB%D1%8C%D1%81%D1%8C%D0%BA%D0%BE%D0%BC%D1%83-%D1%82%D0%BB%D1%96.jpg?s=612x612&w=0&k=20&c=5PMyeCGkOJPm9TZuTxDOe4TnRR7umMVUykW18sLawms=',
   })
@@ -91,7 +83,6 @@ const CocktailDataSchema = SchemaFactory.createForClass(CocktailData);
   toJSON: {
     virtuals: true,
   },
-  // toObject: { virtuals: true },
 })
 export class Cocktail {
   @Prop({
